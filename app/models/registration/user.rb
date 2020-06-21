c1f78862618a274ee2ce00e16e99d0fb32933c17
Jesse Riggins <jriggins@users.noreply.github.com>
2020-06-21 02:04:06 -0500

    Add annotate

--- app/models/registration/user.rb
+++ app/models/registration/user.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: registration_users
+#
+#  id              :integer          not null, primary key
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 module Registration
   class User < ApplicationRecord
     has_secure_password
